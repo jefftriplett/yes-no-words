f3f208abd6da380790529ee6e82d922b72ed1e92
Jeff Triplett <jeff.triplett@gmail.com>
2023-03-06 23:18:13 -0600

    :shirt: Lints

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,7 @@
 import nox
 
 
-@nox.session(
-    python=["3.7", "3.8", "3.9", "3.10", "3.11"],
-    reuse_venv=True
-)
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"], reuse_venv=True)
 def test(session):
     session.install(".")
     session.install("--requirement=requirements.txt")
